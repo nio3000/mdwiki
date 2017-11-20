50a250e946ef181bb45e235281cb5714bb0081c7
kingsj0405 <kingsj0405@naver.com>
2017-11-21 07:56:10 +0900

    Class Design - AuthManager(LDAP)

--- src/auth_manager.py
+++ src/auth_manager.py
@@ -1,5 +1,20 @@
 import ldap
 
+from enum import Enum
+
+
+class ServerResult(Enum):
+    SUCCESS = 200
+    SERVER_NOT_FOUND = 404
+
+
+class LoginResult(ServerResult):
+    WRONG_ID_OR_PASSWORD = 403
+
+
+class RequestAccessResult(ServerResult):
+    DENIED = 403
+
 
 class AuthManager:
     def __init__(self, server="127.0.0.1"):
@@ -9,4 +24,23 @@ class AuthManager:
         """
         self.server = server
 
-    # TODO: Check how to make LDAP client
+    def login(self, id, password):
+        """
+        Login to LDAP server
+        :param id:
+        :param password:
+        :return: LoginResult
+        """
+
+    def logout(self):
+        """
+        Logout from LDAP server
+        :return: ServerResult
+        """
+
+    def request_access(self, file_id):
+        """
+        Request access to the file whose id is file_id
+        :param file_id:
+        :return: RequestAccessResult
+        """
